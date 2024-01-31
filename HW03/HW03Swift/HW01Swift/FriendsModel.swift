7f2a1dae66e28a872c1c27e9c3a4127daafcd80a
Kirill Golovlev <k-ork-a@mail.ru>
2024-01-31 20:20:05 +0300

    ispravil 3-e dz

--- HW03/HW03Swift/HW01Swift/FriendsModel.swift
+++ HW03/HW03Swift/HW01Swift/FriendsModel.swift
@@ -6,7 +6,41 @@
 //  Copyright © 2024 Kirill. All rights reserved.
 //
 
-//import Foundation
+import Foundation
 
-struct Friend: Decodable {
+// MARK: - Welcome
+struct Welcome: Codable {
+    let response: Response
+}
+
+// MARK: - Response
+struct Response: Codable {
+    let count: Int
+    let items: [Item]
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let id: Int
+    let trackCode: String
+    let photo50: String
+    let firstName, lastName: String
+    let canAccessClosed, isClosed: Bool
+    let deactivated: Deactivated?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case trackCode = "track_code"
+        case photo50 = "photo_50"
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case canAccessClosed = "can_access_closed"
+        case isClosed = "is_closed"
+        case deactivated
+    }
+}
+
+enum Deactivated: String, Codable {
+    case banned = "banned"
+    case deleted = "deleted"
 }
